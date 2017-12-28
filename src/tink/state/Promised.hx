8d2a5e9f106066f26d5ee5f0886852adeecb4a2d
Juraj Kirchheim <back2dos@googlemail.com>
2017-12-28 20:17:15 +0100

    OCD.

--- src/tink/state/Promised.hx
+++ src/tink/state/Promised.hx
@@ -9,21 +9,21 @@ enum Promised<T> {
 }
 
 class PromisedTools {
-  public static function next<A, B>(a:Promised<A>, f:Next<A, B>):Promise<B>
+  static public function next<A, B>(a:Promised<A>, f:Next<A, B>):Promise<B>
     return switch a {
       case Loading: Promise.NEVER;
       case Failed(e): e;
       case Done(a): f(a);
     }
     
-  public static function map<A, B>(a:Promised<A>, f:A->B):Promised<B>
+  static public function map<A, B>(a:Promised<A>, f:A->B):Promised<B>
     return switch a {
       case Loading: Loading;
       case Failed(e): Failed(e);
       case Done(a): Done(f(a));
     }
     
-  public static function flatMap<A, B>(a:Promised<A>, f:A->Promised<B>):Promised<B>
+  static public function flatMap<A, B>(a:Promised<A>, f:A->Promised<B>):Promised<B>
     return switch a {
       case Loading: Loading;
       case Failed(e): Failed(e);
