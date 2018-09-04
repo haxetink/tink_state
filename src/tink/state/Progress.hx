40bbbf1f0232fd1be4d69d1483f6e413a31166e6
Kevin Leung <kevinresol@gmail.com>
2018-09-04 13:06:43 +0800

    Rework, put total value into the progress call together

--- src/tink/state/Progress.hx
+++ src/tink/state/Progress.hx
@@ -1,99 +1,194 @@
 package tink.state;
 
+import tink.state.Observable;
+
 using tink.CoreApi;
 
 private typedef Impl<T> = Pair<Option<Float>, State<ProgressType<T>>>;
 
-@:forward(value)
-abstract Progress<T>(Impl<T>) {
-	public var total(get, never):Option<Float>;
-	inline function get_total() return this.a;
+@:forward(result, bind)
+abstract Progress<T>(ProgressObject<T>) from ProgressObject<T> {
+	public static var INIT(default, null):ProgressValue = new Pair(0.0, None);
 	
-	public inline static function trigger<T>(total):ProgressTrigger<T> {
-		return new ProgressTrigger(total);
+	public inline static function trigger<T>():ProgressTrigger<T> {
+		return new ProgressTrigger();
 	}
 	
-	public static function make<T>(total:Option<Float>, f:(Float->Void)->(T->Void)->Void):Progress<T> {
-		var ret = trigger(total);
+	public static function make<T>(f:(Float->Option<Float>->Void)->(T->Void)->Void):Progress<T> {
+		var ret = trigger();
 		f(ret.progress, ret.finish);
 		return ret;
 	}
 	
-	inline function new(progress) {
-		this = progress;
+	@:to
+	public inline function asFuture():Future<T> {
+		return this.result();
+	}
+	
+	@:from
+	static inline function promise<T>(v:Promise<Progress<T>>):Progress<Outcome<T, Error>> {
+		return new PromiseProgress(v);
+	}
+	
+	@:from
+	static inline function future<T>(v:Future<Progress<T>>):Progress<T> {
+		return new FutureProgress(v);
+	}
+	
+	public inline function next(f) {
+		return asFuture().next(f);
 	}
 	
 	@:to
-	public function result():Future<T> {
-		return observe()
-			.getNext(null, function(v) return switch v {
-				case InProgress(_): None;
-				case Finished(v): Some(v);
-			});
+	public inline function observe():Observable<ProgressType<T>> {
+		return this.observe();
+	}
+}
+
+class ProgressTrigger<T> extends ProgressBase<T> {
+	var state:State<ProgressType<T>>;
+	var _result:Future<T>;
+	
+	public function new() {
+		state = new State(InProgress(Progress.INIT));
 	}
 	
-	/**
-	 * Binds a callback to the progress value (A float value from 0 to 1)
-	 * Will automatically dissolve itself once a result is resolved
-	 */
-	public function progress(?opt, f):CallbackLink {
+	public function progress(v:Float, total:Option<Float>) {
+		switch state.value {
+			case Finished(_): // do nothing
+			case InProgress(current):
+				switch total {
+					case Some(t): if(v > t) v = t;
+					case None:
+				}
+				if(v > current.a) state.set(InProgress(new Pair(v, total)));
+		}
+	}
+	
+	public function finish(v:T) {
+		switch state.value {
+			case Finished(_): // do nothing
+			case InProgress(_):
+				state.set(Finished(v));
+		}
+	}
+	
+	override function result():Future<T> {
+		if(_result == null) {
+			_result = observe()
+				.getNext(null, function(v) return switch v {
+					case InProgress(_): None;
+					case Finished(v): Some(v);
+				});
+		}
+		return _result;
+	}
+	
+	override function observe():Observable<ProgressType<T>> {
+		return state.observe();
+	}
+	
+	public inline function asProgress():Progress<T> {
+		return this;
+	}
+}
+
+class FutureProgress<T> extends ProgressBase<T> {
+	var future:Future<Progress<T>>;
+	var state:State<ProgressType<T>>;
+	
+	public function new(future)
+		this.future = future;
+		
+	
+	override function result():Future<T> {
+		return future.flatMap(function(p) return p.result());
+	}
+	
+	override function observe():Observable<ProgressType<T>> {
+		if(state == null) {
+			state = new State(InProgress(Progress.INIT));
+			future.handle(function(p) p.observe().bind({direct: true}, state.set));
+		}
+		return state.observe();
+	}
+}
+
+class PromiseProgress<T> extends ProgressBase<Outcome<T, Error>> {
+	var promise:Promise<Progress<T>>;
+	var state:State<ProgressType<Outcome<T, Error>>>;
+	
+	public function new(promise)
+		this.promise = promise;
+		
+	
+	override function result():Future<Outcome<T, Error>> {
+		return promise.next(function(p) return p.result());
+	}
+	
+	override function observe():Observable<ProgressType<Outcome<T, Error>>> {
+		if(state == null) {
+			state = new State(InProgress(Progress.INIT));
+			promise.handle(function(o) switch o {
+				case Success(p): p.observe().bind({direct: true}, function(v) state.set(switch v {
+					case InProgress(v): InProgress(v);
+					case Finished(v): Finished(Success(v));
+				}));
+				case Failure(e): state.set(Finished(Failure(e)));
+			});
+		}
+		return state.observe();
+	}
+}
+
+interface ProgressObject<T> {
+	function result():Future<T>;
+	function bind(?opt:BindingOptions<ProgressValue>, f:Callback<ProgressValue>):CallbackLink;
+	function observe():Observable<ProgressType<T>>;
+}
+
+class ProgressBase<T> implements ProgressObject<T> {
+	public function result():Future<T> {
+		throw 'not implemented';
+	}
+	
+	public function bind(?opt:BindingOptions<ProgressValue>, f:Callback<ProgressValue>):CallbackLink {
 		var binding:CallbackLink;
+		
+		var opt:BindingOptions<ProgressType<T>> = switch opt {
+			case null: null;
+			case o: {
+				direct: o.direct,
+				comparator: 
+					if(o.comparator == null)
+						null
+					else 
+						function(v1, v2) return switch [v1, v2] {
+							case [InProgress(p1), InProgress(p2)]: o.comparator(p1, p2);
+							case _: false;
+						}
+			}
+		}
 		
 		binding = 
 			observe()
-				.bind(opt, function(v) {
-					f(switch v {
-						case InProgress(v): v;
-						case Finished(_): binding.dissolve(); 1;
-					});
+				.bind(opt, function(v) switch v {
+					case InProgress(p): f.invoke(p);
+					case Finished(_): binding.dissolve();
 				});
 				
 		return binding;
 	}
 	
-	public inline function next(f) {
-		return result().next(f);
+	public function observe():Observable<ProgressType<T>> {
+		throw 'not implemented';
 	}
 	
-	@:to
-	public inline function observe():Observable<ProgressType<T>> {
-		return this.b.observe();
-	}
 }
 
-@:access(tink.state.Progress)
-abstract ProgressTrigger<T>(Impl<T>) {
-	public inline function new(total) {
-		this = new Pair(total, new State(InProgress(0)));
-	}
-	
-	public function progress(v:Float) {
-		switch [this.b.value, this.a] {
-			case [Finished(_), _]: // do nothing
-			case [InProgress(current), Some(total)]:
-				// Don't allow setting progress greater than or equal to `total`
-				// otherwise observers will see a progress of 1 without a finished value
-				if(v > current && v < total) this.b.set(InProgress(v));
-			case [InProgress(current), None]:
-				if(v > current) this.b.set(InProgress(v));
-		}
-	}
-	
-	public function finish(v:T) {
-		switch this.b.value {
-			case Finished(_): // do nothing
-			case InProgress(current):
-				this.b.set(Finished(v));
-		}
-	}
-	
-	@:to
-	public inline function asProgress():Progress<T> {
-		return new Progress(this);
-	}
-}
+typedef ProgressValue = Pair<Float, Option<Float>>;
 
 enum ProgressType<T> {
-	InProgress(v:Float);
+	InProgress(v:ProgressValue);
 	Finished(v:T);
 }
--- tests/TestProgress.hx
+++ tests/TestProgress.hx
@@ -1,39 +1,61 @@
 import tink.state.*;
 
+import tink.state.Progress;
+
+using tink.CoreApi;
+
 @:asserts
 class TestProgress {
   public function new() {}
   
   public function testProgress() {
-    var state = Progress.trigger(Some(1.0));
+    var state = Progress.trigger();
     var progress = state.asProgress();
     
-    asserts.assert(progress.total.match(Some(1.0)));
     var p = 0.;
-    progress.progress({direct: true}, function(v) p = v);
-    state.progress(0.5);
+    progress.bind({direct: true}, function(v) p = v.a);
+    state.progress(0.5, None);
     asserts.assert(p == 0.5);
     state.finish('Done');
-    asserts.assert(p == 1);
     progress.result().handle(function(v) {
       asserts.assert(v == 'Done');
       asserts.done();
     });
     
-    var state = Progress.trigger(None);
-    var progress = state.asProgress();
+    return asserts;
+  }
+  
+  public function testFutureProgress() {
+    var state = Progress.trigger();
+    var progress:Progress<String> = Future.sync(state.asProgress());
     
-    asserts.assert(progress.total.match(None));
     var p = 0.;
-    progress.progress({direct: true}, function(v) p = v);
-    state.progress(0.5);
+    progress.bind({direct: true}, function(v) p = v.a);
+    state.progress(0.5, None);
     asserts.assert(p == 0.5);
     state.finish('Done');
-    asserts.assert(p == 1);
     progress.result().handle(function(v) {
       asserts.assert(v == 'Done');
       asserts.done();
     });
+    
+    return asserts;
+  }
+  
+  public function testPromiseProgress() {
+    var state:ProgressTrigger<String> = Progress.trigger();
+    var progress:Progress<Outcome<String, Error>> = Promise.lift(state.asProgress());
+    
+    var p = 0.;
+    progress.bind({direct: true}, function(v) p = v.a);
+    state.progress(0.5, None);
+    asserts.assert(p == 0.5);
+    state.finish('Done');
+    progress.result().handle(function(v) {
+      asserts.assert(v.match(Success('Done')));
+      asserts.done();
+    });
+    
     return asserts;
   }
 }
