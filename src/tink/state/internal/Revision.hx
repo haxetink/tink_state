5e3d607ab85ae13212741e12a87ea986f5a6f33b
Juraj Kirchheim <back2dos@googlemail.com>
2020-09-02 15:00:57 +0200

    Work around odd initialization order.

--- src/tink/state/internal/Revision.hx
+++ src/tink/state/internal/Revision.hx
@@ -1,10 +1,12 @@
 package tink.state.internal;
 
 abstract Revision(Float) {//TODO: use Int64 where it's faster and perhaps plain Int on python
-  static var counter = .0;
+  static var counter:Float;
   static public final ZERO = new Revision();
-  public inline function new()
+  public function new() {
+    if (Math.isNaN(counter)) counter = .0;// seems to be a problem in initialization order
     this = counter++;
+  }
   @:op(a < b) static function lt(a:Revision, b:Revision):Bool;
   @:op(a <= b) static function lte(a:Revision, b:Revision):Bool;
   @:op(a > b) static function gt(a:Revision, b:Revision):Bool;
