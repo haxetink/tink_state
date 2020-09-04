14ba6d7a0a958d046f210b3a6f163021e4ed4373
Juraj Kirchheim <back2dos@googlemail.com>
2020-09-04 09:37:04 +0200

    More fun with initialization.

--- src/tink/state/internal/Revision.hx
+++ src/tink/state/internal/Revision.hx
@@ -1,13 +1,17 @@
 package tink.state.internal;
 
 abstract Revision(Float) {//TODO: use Int64 where it's faster and perhaps plain Int on python
+  static public inline var ZERO:Revision = cast .0;
   static var counter:Float;
+  #if !eval
   static function __init__()
     counter = .0;
-  static public inline var ZERO:Revision = cast .0;
+  #end
 
-  public function new()
-    this = counter+=1;
+  public function new() {
+    #if eval if (Math.isNaN(counter)) counter = .0; #end
+    this = counter+=1.0;
+  }
 
   @:op(a < b) static function lt(a:Revision, b:Revision):Bool;
   @:op(a <= b) static function lte(a:Revision, b:Revision):Bool;
